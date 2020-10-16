790240a3bd653f854387aa1d07696411cdf369e0
Podolyan98 <68119338+Podolyan98@users.noreply.github.com>
2020-10-16 22:14:39 +0300

    Add files via upload

---
+++ request.py
@@ -0,0 +1,29 @@
+import requests
+from loguru import logger
+
+
+headers = {'accept': 'text/plain',
+           'Accept': 'application/json'}
+
+url = 'https://auth-api.rabota.ua/Login'
+payload = { 'username': 'trak@vesedia.com',
+            'password': 'vtuf12'}
+
+response = requests.post(url, data = payload, headers = headers)
+logger.debug(response)
+
+token = 'Bearer %s' % response.text.replace('"', '')
+logger.debug('Token received')
+logger.debug(token)
+
+url = 'https://employer-api.rabota.ua/apply/list'
+payload = {'vacancyId': 0, 
+           'folderId': 1,
+           'page': 2,
+           'Authorization': token}
+
+headers = {'accept': 'text/plain',
+           'Content-Type': 'application/json'}
+response = requests.post(url, data = payload, headers = headers)
+logger.debug(response)
+logger.debug(response.text)
